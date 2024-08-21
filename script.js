4856074e178a1ea35c48e148af4f18433df94040
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-08-21 08:31:35 +0300

    feat: add playRound function to play one round using the basic game rules.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+let human = getHumanChoice();
+
+let computer = getComputerChoice();
+
+console.log(playRound(human,computer));
+
 function getComputerChoice(){
 
-let choices = ["Rock","Paper","Scissor"];
+let choices = ["Rock","Paper","Scissors"];
 
 randChoice = Math.floor(Math.random() * choices.length);
 
@@ -16,4 +28,32 @@ function getHumanChoice(){
     return promptResult;
 }
 
-console.log(getHumanChoice())
+function playRound(human,computer){
+
+    
+    human = human.toLowerCase();
+    computer = computer.toLowerCase();
+
+   
+
+    if (human == "rock" && computer == "rock" || human == "paper" && computer == "paper" || human == "scissors" && computer == "scissors"){
+
+        return `A Draw! What a coincidence!`;
+    } 
+    else if (human == "rock" && computer == "scissors" || human == "paper" && computer == "rock" || human == "scissors" && computer == "paper"  ){
+
+        humanScore++;
+        return `You win! ${human} beats ${computer}`;
+    }
+    else if (computer== "rock" && human == "scissors" || computer == "paper" && human == "rock" || computer == "scissors" && human == "paper"  ){
+
+        computerScore++;
+        return ` You lose! ${computer} beats ${human} `;
+    }
+ 
+
+}
+
+
+
+
