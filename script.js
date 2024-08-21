501daf16ac5aeac3698958a0caeab98b7699b367
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-08-21 07:55:59 +0300

    feat: add functions to get the human choice and computer choice for the game.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+
+
+function getComputerChoice(){
+
+let choices = ["Rock","Paper","Scissor"];
+
+randChoice = Math.floor(Math.random() * choices.length);
+
+return choices[randChoice];
+
+}
+
+function getHumanChoice(){
+    let promptResult = prompt("Rock! Paper! Scissors! Choose!");
+
+    return promptResult;
+}
+
+console.log(getHumanChoice())
